f1c5611e19525bf137842b0f359b5ce93f0c4db9
Mustafa Sevgi <msevgi@netas.com.tr>
2014-11-13 16:54:10 +0200

    Initial commit

---
+++ app/src/main/java/com/msevgi/facebookapiex/MyActivity.java
@@ -0,0 +1,31 @@
+
+package com.msevgi.facebookapiex;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MyActivity extends FragmentActivity {
+
+   private MainFragment mainFragment;
+
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+      if (savedInstanceState == null) {
+         // Add the fragment on initial activity setup
+         mainFragment = new MainFragment();
+         getSupportFragmentManager().beginTransaction().add(android.R.id.content, mainFragment).commit();
+      }
+      else {
+         // Or set the fragment from restored state info
+         mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(android.R.id.content);
+      }
+   }
+
+   // @Override
+   // public boolean onCreateOptionsMenu(Menu menu) {
+   // getMenuInflater().inflate(R.menu.main, menu);
+   // return true;
+   // }
+}
