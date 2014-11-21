7022af9372d68ce1f9dfcf2b09a58cd4a40b2b69
Mustafa Sevgi <msevgi@netas.com.tr>
2014-11-21 10:40:41 +0200

    face image loaded

--- app/src/main/java/com/msevgi/facebookapiex/MainFragment.java
+++ app/src/main/java/com/msevgi/facebookapiex/MainFragment.java
@@ -1,6 +1,7 @@
 package com.msevgi.facebookapiex;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -8,24 +9,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.HttpMethod;
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
-import com.facebook.model.GraphUser;
+import com.facebook.model.GraphObject;
 import com.facebook.widget.LoginButton;
-import com.facebook.widget.ProfilePictureView;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class MainFragment extends Fragment {
 
     private static final String TAG = "MainFragment";
 
     private UiLifecycleHelper uiHelper;
-
     private Session.StatusCallback callback = new Session.StatusCallback() {
         @Override
         public void call(final Session session,
@@ -35,11 +37,12 @@ public class MainFragment extends Fragment {
         }
     };
 
-    private ProfilePictureView imageView;
+//    private ImageView imageView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
-        imageView=(ProfilePictureView) view.findViewById(R.id.imageview_profile);
+//        imageView = (ImageView) view.findViewById(R.id.imageview_profile);
         LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
         authButton.setFragment(this);
         // authButton.setReadPermissions(Arrays.asList("user_likes", "user_status"));
@@ -96,33 +99,83 @@ public class MainFragment extends Fragment {
     private void onSessionStateChange(final Session session, SessionState state, Exception exception) {
         if (state.isOpened()) {
             Log.i(TAG, "Logged in...");
-            Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
-                @Override
-                public void onCompleted(GraphUser user, Response response) {
-                    // If the response is successful
+            Bundle params = new Bundle();
+            params.putBoolean("redirect", false);
+            params.putString("height", "200");
+            params.putString("type", "normal");
+            params.putString("width", "200");
+/* make the API call */
+            new Request(
+                    session,
+                    "/me/picture",
+                    params,
+                    HttpMethod.GET,
+                    new Request.Callback() {
+                        public void onCompleted(Response response) {
+            /* handle the result */
+                            Log.i("mustafa", response.toString());
+                            GraphObject responseGraphObject = response.getGraphObject();
 
-                    if (session == Session.getActiveSession()) {
-                        if (user != null) {
-                            Log.i("userd id", "mustafa " + user.getId());
-                            Log.i("name view", user.getName());
-                            BusProvider.getInstance().post(new Model(user.getId()));
+                            //Create the JSON object
+                            JSONObject json = responseGraphObject.getInnerJSONObject();
                             try {
-                                URL image_value = new URL("http://graph.facebook.com/"+ user.getId()+ "/picture?type=large");
-//                                Picasso.with(getActivity().getApplicationContext()).load(image_value).
-                            } catch (MalformedURLException e) {
+                                String profilImageUrl=json.getJSONObject("data").getString("url");
+                                Log.i("mustafa profil url",profilImageUrl);
+                                BusProvider.getInstance().post(new Model(profilImageUrl));
+//                                Picasso.with(getActivity().getApplicationContext()).load(profilImageUrl).into(imageView);
+                            } catch (JSONException e) {
                                 e.printStackTrace();
                             }
-//                            user_ID = user.getId();//user id
-//                            profileName = user.getName();//user's profile name
-//                            userNameView.setText(user.getName());
+
                         }
                     }
-                }
-            });
-            Request.executeBatchAsync(request);
+            ).executeAsync();
+
+//            Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
+//                @Override
+//                public void onCompleted(final GraphUser user, Response response) {
+//                    // If the response is successful
+//
+//                    if (session == Session.getActiveSession()) {
+//                        if (user != null) {
+//                            Log.i("userd id", "mustafa " + user.getId());
+//                            Log.i("name view", user.getName());
+//                            BusProvider.getInstance().post(new Model(user.getId()));
+//                            imageView.setProfileId(user.getId());
+//                           // new Async("http://graph.facebook.com/" + user.getId()).execute();
+//                        }
+////                            user_ID = user.getId();//user id
+////                            profileName = user.getName();//user's profile name
+////                            userNameView.setText(user.getName());
+//
+//                    }
+//                }
+//            });
+//            Request.executeBatchAsync(request);
 
         } else if (state.isClosed()) {
             Log.i(TAG, "Logged out...");
+        }
+    }
+
+    private class Async extends AsyncTask<Void, Void, Void> {
+        private String url;
+
+        public Async(String url) {
+            this.url = url;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                JSONObject json = JsonReader.readJsonFromUrl(url);
+                Log.i("mustafa", "mustafa" + json.getString("url"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 }
--- app/src/main/java/com/msevgi/facebookapiex/MyActivity.java
+++ app/src/main/java/com/msevgi/facebookapiex/MyActivity.java
@@ -2,29 +2,32 @@ package com.msevgi.facebookapiex;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.ImageView;
 
-import com.facebook.widget.ProfilePictureView;
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
+import com.squareup.picasso.Picasso;
 
 public class MyActivity extends FragmentActivity {
 
     private MainFragment mainFragment;
-private ProfilePictureView imageView;
+    private ImageView imageView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
         getBus().register(this);
-        imageView=(ProfilePictureView) findViewById(R.id.imageview_profile);
-        if (savedInstanceState == null) {
-            // Add the fragment on initial activity setup
-            mainFragment = new MainFragment();
-            getSupportFragmentManager().beginTransaction().add(android.R.id.content, mainFragment).commit();
-        } else {
-            // Or set the fragment from restored state info
-            mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(android.R.id.content);
-        }
+
+//        if (savedInstanceState == null) {
+//            // Add the fragment on initial activity setup
+//            mainFragment = new MainFragment();
+//            getSupportFragmentManager().beginTransaction().add(android.R.id.content, mainFragment).commit();
+//        } else {
+//            // Or set the fragment from restored state info
+//            mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(android.R.id.content);
+//        }
+        imageView = (ImageView) findViewById(R.id.imageview_profile);
     }
 
     @Override
@@ -44,8 +47,8 @@ private ProfilePictureView imageView;
 
     @Subscribe
     public void getId(Model user_id) {
-       imageView.setProfileId(user_id.getUser_id());
-//        Picasso.with(getApplicationContext()).load("http://graph.facebook.com/" + user_id.getUser_id() + "/picture").into(imageView);
+//       imageView.setProfileId(user_id.getUser_id());
+        Picasso.with(getApplicationContext()).load(user_id.getUser_id()).into(imageView);
 
     }
 }
